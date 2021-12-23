025506b388fb6de51571c9606c3110b60b8ddc41
Akshit <cryptoalertcan@gmail.com>
2021-12-23 13:16:10 +0530

    Alert Changes!

--- js/alerts.js
+++ js/alerts.js
@@ -17,3 +17,51 @@ function logout()
     firebase.auth().signOut()
     location.reload()
 }
+
+Notification.requestPermission(permission => {
+    if(permission === 'granted') {
+        // show notification
+    }
+});
+
+
+function CreateAlert()
+{
+    setInterval( function() {
+
+        var ptr = document.getElementById("aprice").value;
+        if(ptr=='')
+        {
+            ptr = 100000000000000;
+        }
+    
+        var str = document.getElementById("bprice").value;
+        if(str=='')
+        {
+            str = 0;
+        }
+        console.log(str);
+        console.log(ptr);
+    
+        var price;
+        fetch('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC&tsyms=USD')
+        .then(res => res.json()) 
+        .then(data => price = data.BTC.USD)
+        .then(() => { console.log(price)
+        if(price > ptr)
+        {
+        var myNoti = new Notification('CryptoAlert', {
+            body: "ALERT: BTC price is now GREATER than your alert price.                  Current price: " +price + " INR"
+        });
+        document.getElementById("aprice").value = null;
+        }
+        else if(price < str)
+        {
+        var myNoti = new Notification('CryptoAlert', {
+            body: "ALERT: BTC price is now LESS than your alert price.                        Current price: " +price + " INR"
+        });
+        document.getElementById("bprice").value = null;
+    }
+    });
+    },1000);
+}
